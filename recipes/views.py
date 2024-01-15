15d2555de21e35acd604022e88cc1e25a48916f4
Nícolas Alencar de Araújo <titanalenujo@gmail.com>
2024-01-15 18:22:11 -0300

    Injetando dados na lista de view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Nícolas Alencar',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id)  :
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Nícolas Alencar',
+        'recipe': make_recipe(),
     })
