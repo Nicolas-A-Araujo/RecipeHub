c96a6707a656100f65662f5a4fc1bdc5cef2bc67
Nícolas Alencar de Araújo <titanalenujo@gmail.com>
2024-01-17 17:08:24 -0300

    Registrando category na admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
