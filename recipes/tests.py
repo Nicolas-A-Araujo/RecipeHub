c1c342955f9d06fae9abc4424f86ccfee503e719
Nícolas Alencar de Araújo <titanalenujo@gmail.com>
2024-01-22 10:38:07 -0300

    Implemetando o pytest e seus fundamentos

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class RecipeUrlsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        variable = '13212'
+        print('Hello World')
+        assert 1 == 1, '1 é igual a 1'
