f6a03777dcf680c168e0742a5ffef47bc33f0c5e
Nícolas Alencar de Araújo <titanalenujo@gmail.com>
2024-01-23 20:58:32 -0300

    Implementado test da view function

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+from recipes import views
 
 class RecipeUrlsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
@@ -13,3 +14,23 @@ class RecipeUrlsTest(TestCase):
     def test_recipe_detail_home_url_is_correct(self):
         home_url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(home_url, '/recipes/1/')
+
+
+class RecipeViewTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:home')
+        )
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1})
+        )
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        self.assertIs(view.func, views.recipe)
