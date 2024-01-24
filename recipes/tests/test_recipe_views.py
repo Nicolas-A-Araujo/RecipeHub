fa5e19b0ee9099c91e63a5c3b46f6edcaf53440a
Nícolas Alencar de Araújo <titanalenujo@gmail.com>
2024-01-23 21:29:11 -0300

    Separação de testes em um package tests

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -2,20 +2,6 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
 
-class RecipeUrlsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipe_category_home_url_is_correct(self):
-        home_url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(home_url, '/recipes/category/1/')
-
-    def test_recipe_detail_home_url_is_correct(self):
-        home_url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(home_url, '/recipes/1/')
-
-
 class RecipeViewTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(
