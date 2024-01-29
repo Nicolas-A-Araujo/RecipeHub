9ccc5de83179c0b3f00774fd37c239ea799fbeac
Nícolas Alencar de Araújo <titanalenujo@gmail.com>
2024-01-29 13:34:46 -0300

    Implementação do pacote parameterized para test max_length

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,13 +1,19 @@
-from .test_recipe_base import RecipeTestBase
 from django.core.exceptions import ValidationError
+from parameterized import parameterized
+from .test_recipe_base import RecipeTestBase
 
 class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
         self.recipe = self.make_recipe()
         return super().setUp()
-    
-    def test_recipe_title_has_raises_error_for_if_title_has_more_than_65_chars(self):
-        self.recipe.title = 'N' * 70
 
+    @parameterized.expand([
+            ('title', 65),
+            ('description', 165),
+            ('preparation_time_unit', 65),
+            ('servings_unit', 65)
+        ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'N' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
