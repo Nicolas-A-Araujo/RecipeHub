1d7012b93f0eef1cd628baef5ff935e5c31b06a9
Nícolas Alencar de Araújo <titanalenujo@gmail.com>
2024-01-29 12:33:48 -0300

    Implementação de test max_lenght do title

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,6 +1,13 @@
 from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
 
 class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
         self.recipe = self.make_recipe()
         return super().setUp()
+    
+    def test_recipe_title_has_raises_error_for_if_title_has_more_than_65_chars(self):
+        self.recipe.title = 'N' * 70
+
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
