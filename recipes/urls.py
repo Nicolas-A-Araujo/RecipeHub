313da79131a40c44644a2ad72cf5443d142590f0
Nícolas Alencar de Araújo <titanalenujo@gmail.com>
2024-02-02 23:37:34 -0300

    Docs

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,7 +6,7 @@ app_name =  'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"), #Home
-    path('recipes/search/', views.search, name="search"),
-    path('recipes/category/<int:category_id>/', views.category, name="category"),
-    path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path('recipes/search/', views.search, name="search"), # Search
+    path('recipes/category/<int:category_id>/', views.category, name="category"), # Category
+    path('recipes/<int:id>/', views.recipe, name="recipe"), # Recipe
 ]
